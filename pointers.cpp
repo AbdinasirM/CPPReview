376cf257a66e72ae7c12e0499cc49aaa552bd8fe
AbdinasirM <aabdi5520@gmail.com>
2024-09-10 22:15:20 -0500

    pointers in c++

---
+++ pointers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 9;
+    cout<<age<<endl; //outputs the value of the variable back
+    cout<<&age<<endl; //outputs the address of where this variable is saved and also the adrress keeps changing randomly for security reasons
+
+
+    return 0;
+}
